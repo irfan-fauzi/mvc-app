0f52c209912f8d9d689c0cd2893d0466ef51b360
irfan-fauzi <irfanwintech@yahoo.com>
2025-09-21 23:05:30 +0700

    fix error isset on class App

--- app/core/App.php
+++ app/core/App.php
@@ -1,9 +1,56 @@
 <?php
+
+  /*
+    Dokumentasi
+    aplikasi ini berbasis url, fokus pada url
+    misal pada url di browser = http://localhost/mvc-app/transaksi/pembayaran/tunai
+    maka = http://localhost/mvc-app/controller/method/parameter
+    jadi kelas dibawah ini fungsinya untuk mengisi controller, method, dan param
+    menjadi modular/fleksibel tergantung url yang diketik user
+
+
+
+  */
+
   class App {
+
+  protected $controller = 'Home';
+  protected $method = 'index';
+  protected $params = [];
+
   public function __construct()
   {
     $url = $this->pareseUrl();
-    var_dump($url);
+    // ketika mengetik url di browser, urutannya /controller/method misal 'home/index' home=controller index=method
+    // cek apakah ada file didalam folder controller yang namanya sesuai dengan apa yang di tulis di url
+    if (isset($url[0])) {
+      if (file_exists('../app/controllers/' . $url[0] . '.php')) {
+        // kalau ada, timpa controller $controller di atas jadi controller baru yang di input
+        $this->controller = $url[0];
+        unset($url[0]);
+
+      }
+    }
+
+    require_once '../app/controllers/' . $this->controller .'.php';
+    $this->controller = new $this->controller(); // instansiasi object nya
+    // cek dulu method nya ada ga didalam controllernya
+    if (isset($url[1])) {
+      if(method_exists($this->controller, $url[1])){
+        $this->method = $url[1];
+        unset($url[1]);
+      }
+    }
+
+    // mengisi $parameter diatas menjadi sesuai dengan url yang diketik 
+    if(!empty($url)){
+      $this->params = array_values($url);
+    }
+
+    // jalankan controller &method, kirimkan params jika ada
+    call_user_func_array([$this->controller, $this->method], $this->params);
+
+
   }
     
   // untuk menjalankan controller, tangkap url untuk di olah di controller
@@ -16,4 +63,8 @@
     }
   }
 }
+
+
+
+		
 ?>
