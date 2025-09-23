cee820e18afcf148930670ac07846363e29ac144
irfan-fauzi <irfanwintech@yahoo.com>
2025-09-23 10:30:11 +0700

    success view

--- app/controllers/About.php
+++ app/controllers/About.php
@@ -1,7 +1,7 @@
 <?php
 class About {
   public function index($name, $price){
-    echo 'about/index = name:'. $name. 'price:'. $price;
+    echo 'name:'. $name. 'price:'. $price;
   }
   public function page() {
     echo 'about/page';
