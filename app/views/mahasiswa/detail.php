a616f5ba3a47ef275d83058e71300183e5b109e6
irfan-fauzi <irfanwintech@yahoo.com>
2025-09-29 12:32:32 +0700

    oke yang penting faham konsep mvc

--- app/views/mahasiswa/detail.php
+++ app/views/mahasiswa/detail.php
@@ -6,13 +6,11 @@
 <main class="container-lg mx-auto">
   <h1 class="text-3xl">Detail Mahasiswa page</h1>
    <ul>
-            <li class=" border-b p-2">
-                <strong>Nama:</strong><?= $mhs["nama"]; ?>  <br> 
-                <strong>Email:</strong> <?= $mhs["email"]; ?> <br> 
-                <strong>Jurusan:</strong> <?= $mhs["jurusan"]; ?> <br> 
-    
-            </li>
-            <br>
-      
+      <li class=" border-b p-2">
+         <strong>Nama:</strong><?= $mhs["nama"]; ?>  <br> 
+         <strong>Email:</strong> <?= $mhs["email"]; ?> <br> 
+         <strong>Jurusan:</strong> <?= $mhs["jurusan"]; ?> <br> 
+      </li>
+      <br>  
     </ul>
 </main>
